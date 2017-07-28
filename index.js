7718859b65b87f8ff84412633f783412f7a3a4eb
paulorodrigues2 <p_asr@hotmail.com>
2017-07-28 19:39:31 +0100

    fix error

--- index.js
+++ index.js
@@ -99,7 +99,11 @@ app.use("*",function(req,res){
 
 
 
-app.listen(3001,function(){
+app.listen(process.env.PORT || 3001,function(){
   console.log("Live at Port 3001");
 });
 
+/*
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});*/
