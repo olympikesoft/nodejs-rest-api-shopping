0e6e7556c22df4649a8bc1df2de9b53dff2623ba
paulorodrigues2 <p_asr@hotmail.com>
2017-12-27 09:44:37 +0000

    change

--- index.js
+++ index.js
@@ -198,7 +198,3 @@ app.listen(port,function(){
   console.log("Live at Port 3000");
 });
 
-/*
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});*/
